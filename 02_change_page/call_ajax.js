782847b38d7b4f85fae91fc5fce7338c89944adb
Justin <juns3700@gmail.com>
2016-01-27 01:26:12 +0800

    Add 02 example

--- 02_change_page/call_ajax.js
+++ 02_change_page/call_ajax.js
@@ -1,6 +1,9 @@
 /* jQuery AJAX 範例 */
 
 var getSelNews = function (urlSrc) {
+    var curr_page = $('#currPage').val();  //目前頁數:1 (default)
+    var per_page  = $('#perPage').val();   //每頁幾筆:5 (default)
+    var urlSrc    = 'http://news.housefun.com.tw/api/partner/GetNewsListByPage/-/1/' + per_page + '/' + curr_page;
     $.ajax({
         url          : urlSrc,
         type         : "GET",
@@ -11,9 +14,10 @@ var getSelNews = function (urlSrc) {
 
 /* callback 寫這裡 */
 var getSelNewsCallback = function (data){
-    if(data.code === 200) {
+    $('.content').html(""); //清空div
+    if (data.code === 200) {
         for (var i=0; i<data.result.length; i++) {
-        $('body').append('<div>新聞標題：' + 
+            $('.content').append('<div>[' + (i+1) + ']新聞標題：' + 
                 data.result[i].title +
                 '<br><br>' + data.result[i].brief + 
                 '<br>----------------------------------------------------------------------------</div>');
@@ -21,9 +25,10 @@ var getSelNewsCallback = function (data){
     }
 };
 
-/* 初始化 丟網址當作參數進去 */
-var urlSrc = 'http://news.housefun.com.tw/api/partner/GetNewsListByPage/-/1/5/1';
-/*
-5/1 一頁5筆/第1頁
-*/
-getSelNews(urlSrc);
+/* 按下 Get News 按鈕時 */
+$('#getNewsBtn').on('click', function(){
+    getSelNews();
+});
+
+
+
