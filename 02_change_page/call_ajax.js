71a7995e4afdd492174fc647f21cf2df4dfbcb61
Justin <juns3700@gmail.com>
2016-01-29 00:02:42 +0800

    update

--- 02_change_page/call_ajax.js
+++ 02_change_page/call_ajax.js
@@ -1,38 +1,46 @@
 /* jQuery AJAX 範例 */
 
-var getSelNews = function () {
-    var curr_page = $('#currPage').val();  //目前頁數:1 (default)
-    var per_page  = $('#perPage').val();   //每頁幾筆:5 (default)
-    var urlSrc    = 'http://news.housefun.com.tw/api/partner/GetNewsListByPage/-/1/' + per_page + '/' + curr_page;
-    $.ajax({
-        url          : urlSrc,
-        type         : "GET", //'GET' or 'POST'
-        dataType     : "jsonp",
-        jsonpCallback: "getSelNewsCallback"
-    });
-};
+/* 先在全域變數宣告兩個 function */
+var getSelNews         = {};
+var getSelNewsCallback = {};
 
-/* callback 寫這裡 */
-var getSelNewsCallback = function (data){
-    $('.content').html(""); //清空div
-    if (data.code === 200) {
-        for (var i=0; i<data.result.length; i++) {
-            $('.content').append('<div class="news">[' + (i+1) + ']新聞標題：' + 
-                data.result[i].title +
-                '<br><br>' + data.result[i].brief + 
-                '<br>----------------------------------------------------------------------------</div>');
+/* document ready 後執行下面 jQuery 的 function */
+$(function(){
+
+    /* 這邊不寫 var 不宣告, 直接複寫全域的function */
+    getSelNews = function () {
+        var curr_page = $('#currPage').val();  //目前頁數:1 (default)
+        var per_page  = $('#perPage').val();   //每頁幾筆:5 (default)
+        var urlSrc    = 'http://news.housefun.com.tw/api/partner/GetNewsListByPage/-/1/' + per_page + '/' + curr_page;
+        $.ajax({
+            url          : urlSrc,
+            type         : "GET", //'GET' or 'POST'
+            dataType     : "jsonp",
+            jsonpCallback: "getSelNewsCallback"
+        });
+    };
+
+    /* 這邊不寫 var 不宣告, 直接複寫全域的function */
+    getSelNewsCallback = function (data){
+        $('.content').html(""); //清空div
+        if (data.code === 200) {
+            for (var i=0; i<data.result.length; i++) {
+                $('.content').append('<div class="news">[' + (i+1) + ']新聞標題：' + 
+                    data.result[i].title +
+                    '<br><br>' + data.result[i].brief + 
+                    '<br>----------------------------------------------------------------------------</div>');
+            }
+        } else {
+            alert('ajax fail! code: ' + data.code);
         }
-    } else {
-        alert('ajax fail! code: ' + data.code);
-    }
-};
+    };
 
-/* 按下 Get News 按鈕時 */
-$('#getNewsBtn').on('click', function(){
+    /* 按下 Get News 按鈕時 */
+    $('#getNewsBtn').on('click', function(){
+        getSelNews();
+    });
+
+    /* init */
     getSelNews();
+
 });
-
-/* init */
-getSelNews();
-
-
--- 03_jQuey_ajax_done/call_ajax.js
+++ 03_jQuey_ajax_done/call_ajax.js
@@ -1,33 +1,37 @@
 /* jQuery AJAX .done() 範例 可以比對 02 example 的寫法 */
 
-/* 按下 Get News 按鈕時 */
-$('#getNewsBtn').on('click', function(){
-    var curr_page = $('#currPage').val();  //目前頁數:1 (default)
-    var per_page  = $('#perPage').val();   //每頁幾筆:5 (default)
-    var urlSrc    = 'http://news.housefun.com.tw/api/partner/GetNewsListByPage/-/1/' + per_page + '/' + curr_page;
+/* document ready 後執行下面 jQuery 的 function */
+$(function(){
+    
+    /* 按下 Get News 按鈕時 */
+    $('#getNewsBtn').on('click', function(){
+        var curr_page = $('#currPage').val();  //目前頁數:1 (default)
+        var per_page  = $('#perPage').val();   //每頁幾筆:5 (default)
+        var urlSrc    = 'http://news.housefun.com.tw/api/partner/GetNewsListByPage/-/1/' + per_page + '/' + curr_page;
 
-    /* $.ajax() 後面寫 done 的 call back */
-    $.ajax({
-        url     : urlSrc, 
-        dataType: "jsonp"
-    })
-    .done(function(data) {
-        $('.content').html(""); //清空div
-        if (data.code === 200) {
-            for (var i=0; i<data.result.length; i++) {
-                $('.content').append('<div>[' + (i+1) + ']新聞標題：' + 
-                    data.result[i].title +
-                    '<br><br>' + data.result[i].brief + 
-                    '<br>----------------------------------------------------------------------------</div>');
+        /* $.ajax() 後面寫 done 的 call back */
+        $.ajax({
+            url     : urlSrc, 
+            dataType: "jsonp"
+        })
+        .done(function(data) {
+            $('.content').html(""); //清空div
+            if (data.code === 200) {
+                for (var i=0; i<data.result.length; i++) {
+                    $('.content').append('<div>[' + (i+1) + ']新聞標題：' + 
+                        data.result[i].title +
+                        '<br><br>' + data.result[i].brief + 
+                        '<br>----------------------------------------------------------------------------</div>');
+                }
             }
-        }
-    })
-    /* 這邊可以寫 fail 的 callback (可不寫)*/
-    .fail(function(){
-        console.log('get news fail');
-    })
-    /* 這邊可以寫 always 的 callback (可不寫)*/
-    .always(function(){
-        console.log('不論成功或是失敗這個 callback always 觸發！');
+        })
+        /* 這邊可以寫 fail 的 callback (可不寫)*/
+        .fail(function(){
+            console.log('get news fail');
+        })
+        /* 這邊可以寫 always 的 callback (可不寫)*/
+        .always(function(){
+            console.log('不論成功或是失敗這個 callback always 觸發！');
+        });
     });
 });
