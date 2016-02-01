b133f2415da8d7132c12381389b0ab6ffb20cbf2
Justin Wu <juns3700@gmail.com>
2016-02-02 00:55:30 +0800

    Update intro-for-loop.js

--- intro-for-loop.js
+++ intro-for-loop.js
@@ -23,6 +23,34 @@ document.write (s);
 
 
 
+// Initialize the array.
+var arr = new Array("zero","one","two");
+
+// Add a few expando properties to the array.
+arr["orange"] = "fruit";
+arr["carrot"] = "vegetable";
+
+// Iterate over the properties and elements.
+var s = "";
+for (var key in arr) {
+    s += key + ": " + arr[key];
+    s += "<br />";
+}
+
+document.write (s);
+
+// Output:
+//   0: zero
+//   1: one
+//   2: two
+//   orange: fruit
+//   carrot: vegetable
+
+
+
+
+
+
 
 var myMusic = {
   Mayday : ["01-擁抱","02-純真","03-尬車"],
